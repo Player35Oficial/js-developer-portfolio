8e587b23e2e08587373be147989b2711dbc1128f
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-02-01 23:11:17 -0300

    Preenchendo os dados do Perfil

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-  const url = 'https://raw.githubusercontent.com/Player35Oficial/js-developer-portfolio/main/data/profile.json'
+  const url = 'https://raw.githubusercontent.com/Player35Oficial/js-developer-portfolio/projeto-base/data/profile.json'
 
   const fetching = await fetch(url)
   return await fetching.json()
