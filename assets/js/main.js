722a66b7c0545ac9a840323ca701863a6486350a
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-02-02 16:49:58 -0300

    Preenchendo Idiomas

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,10 +40,16 @@ function updateHardSkills(profileData) {
   </li>`).join('')
 }
 
+function updateLanguages(profileData) {
+  const languages = document.getElementById('profile.languages')
+  languages.innerHTML = profileData.idiomas.map(language => `<li>${language}</li>`).join('')
+}
+
 (async () => {
 
   const profileData = await fetchProfileData()
   updateProfileInfo(profileData)
   updateSoftSkills(profileData)
   updateHardSkills(profileData)
+  updateLanguages(profileData)
 })()
